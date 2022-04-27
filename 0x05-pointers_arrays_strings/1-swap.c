2aa3b701e6c4de61be881724334472eb93573e7d
blak-beryl <54101436+blak-beryl@users.noreply.github.com>
2022-04-27 21:23:40 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-  * swap_int - swaps the values
-  *@a: pointer to a
-  *@b: ponter to b
-  *Return: Nothing
-  */
+* swap_int - swaps the values
+*@a: pointer to a
+*@b: ponter to b
+*Return: Nothing
+*/
 void swap_int(int *a, int *b)
 {
-		int aux;
+	int aux;
 
-			aux = *a;
-				*a = *b;
-					*b = aux;
+	aux = *a;
+	*a = *b;
+	*b = aux;
 }
